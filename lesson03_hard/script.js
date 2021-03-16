5e8aa39abe548920816542af197de07a13a8b6d7
Dmitry Avilov <d.av89@yandex.ru>
2021-03-16 20:11:44 +0300

    lesson03 completed

--- lesson03_hard/script.js
+++ lesson03_hard/script.js
@@ -1,20 +1,61 @@
-let num = 266219,
-    sNum = num.toString(),
-    numArr = [];
+'use strict';
 
-for (let i=0; i<sNum.length; i++) {
-    numArr.push(sNum[i]);
-}
+const lang = prompt('Введите язык', 'ru'),
+      daysRu = `
+        <ul class="days">
+            <li class="day">Понедельник</li>
+            <li class="day">Вторник</li>
+            <li class="day">Среда</li>
+            <li class="day">Четверг</li>
+            <li class="day">Пятница</li>
+            <li class="day">Суббота</li>
+            <li class="day">Воскресенье</li>
+        </ul>
+      `,
+      daysEn = `
+        <ul class="days">
+            <li class="day">Sunday</li>
+            <li class="day">Monday</li>
+            <li class="day">Tuesday</li>
+            <li class="day">Wednesday</li>
+            <li class="day">Thursday</li>
+            <li class="day">Friday</li>
+            <li class="day">Saturday</li>
+        </ul>
+      `;
 
-for (let i=0; i<numArr.length; i++) {
-    numArr[i] = +numArr[i]
-}
+const toDay = () => {
+    const wrapper = document.querySelector('.wrapper');
 
-let result = eval(numArr.join('*'))
+    const withIf = () => {
+         if (lang === 'ru') {
+            wrapper.innerHTML = daysRu;
+        } else {
+            wrapper.innerHTML = daysEn;
+        }
+    }
 
-console.log(result);
+    const withSwitch = () => {
+        switch (lang) {
+            case 'ru':
+                wrapper.innerHTML = daysRu;
+                break;
+            case 'en':
+                wrapper.innerHTML = daysEn;
+                break;
+        };
+    };
 
-result = result ** 3;
-result = result.toString();
+    const withTernary = () => {
+        const whatLang = lang === 'ru' ? ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
+        : 
+        ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-console.log(result.substr(0, 2));
+        wrapper.innerHTML = whatLang.join(', ');
+    }
+
+    withTernary();
+};
+
+toDay();
+
