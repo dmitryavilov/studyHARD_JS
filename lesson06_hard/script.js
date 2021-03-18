8601fe3f29abbef4a4da6fec30a401c51375e70c
Dmitry Avilov <d.av89@yandex.ru>
2021-03-18 19:04:06 +0300

    lesson06 completed

--- lesson06_hard/script.js
+++ lesson06_hard/script.js
@@ -1,35 +1,70 @@
-'use strict';
 
-const arr = ['233331', '42123' , '63123', '84112312', '11233', '42111', '532322'];
+    'use strict';
 
-const checkArr = () => {
-    const wrapper = document.querySelector('.wrapper');
-    let counter = 1;
-
-    arr.forEach(item => {
-        if (item[0] === '2' || item[0] === '4') {
-            wrapper.innerHTML += `${counter}. ${item} <br/>`;
-            counter++;
-        }
-    });
-};
-
-checkArr();
-
-const checkNum = () => {
-    const wrapper = document.querySelector('.wrapper-2');
-    
-    for (let i = 2; i <= 100; i++) {
-        let flag = 1;
-    
-        for (let j = 2; (j <= i/2) && (flag === 1); j = j + 1) {
-            if (i % j === 0) {
-                flag = 0;
-            };
+    const toPlay = () => {
+        const getNumber = (min, max) => {
+            let rand = min - 0.5 + Math.random() * (max - min + 1);
+            return Math.round(rand);
         };
-    
-        if (flag === 1) wrapper.innerHTML += `${i} - делители 1 и ${i} <br/>`;
-    }
-}
 
-checkNum();
+        const randomNumber = getNumber(0, 100);
+        let tryCounter = 10;
+        
+        const checkNumber = () => {
+            const userNumber = prompt('Введите любое число от 0 до 100', 53);
+            
+            switch (true) {
+                case (userNumber === null):
+                    alert('Игра окончена!');
+                    break;
+                case (userNumber === ''):
+                    alert('Не оставляйте поле пустым');
+                    checkNumber();
+                    break;
+                case (tryCounter < 1):
+                    const playCase = confirm('Попытки закончились, хотите сыграть еще?');
+
+                    if (playCase) {
+                        tryCounter = 10;
+                        checkNumber();
+                    } else {
+                        alert('Игра окончена!');
+                    };
+
+                    break;
+                case (+userNumber > 100):
+                    alert('Введите число в указанном диапазоне!');
+                    checkNumber();
+                    break;
+                case isNaN(+userNumber):
+                    alert('Введите число в верном формате');
+                    checkNumber();
+                    break;
+                case (+userNumber > randomNumber):
+                    tryCounter--;
+                    alert(`Загаданное число меньше, осталось попыток ${tryCounter}`);
+                    checkNumber();
+                    break;
+                case (+userNumber < randomNumber):
+                    tryCounter--;
+                    alert(`Загаданное число больше, осталось попыток ${tryCounter}`);
+                    checkNumber();
+                    break;
+                case (+userNumber === randomNumber):
+                    const congratulations = confirm('Поздравляю, Вы угадали! Хотите сыграть еще');
+
+                    if (congratulations) {
+                        tryCounter = 10;
+                        checkNumber();
+                    } else {
+                        alert('Игра окончена!');
+                    };
+
+                    break;
+            }
+        };
+
+        checkNumber();
+    };
+
+    toPlay();
